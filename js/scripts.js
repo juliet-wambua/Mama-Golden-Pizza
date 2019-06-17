90e57e798f9a15ea8f05af87d57dfb9c47521115
juliet-wambua <julienea289@gmail.com>
2019-06-17 15:46:10 +0300

    Add functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,3 @@
-//Business Logic
-var myOrder = function(size) {
-  if (pizza-size==="1"){
-    return "small"
-  }else if (pizza-size==="2") {
-    return "medium"
-  }else if (pizza-size==="3"){
-    retuen "large"
-  }
-}
-var counter = 0
-
-function deliver(size, name){
-  this.delivery = [];
-  this.size = size;
-  this.cost = 950;
-  this.name = name;
-}
-deliver.prototype.toppings = function(array){
-  for(i=0;i<array.length;i++) {
-    this.delivery.push(parseInt(array[i]));
-  }
-}
-
 
 // User interface logic
 $(document).ready(function(){
@@ -49,14 +25,55 @@ $(document).ready(function(){
     $(".large-icon").show();
     $(".large").hide();
   });
-
-
-
-
-
-
-
-
-
-
 });
+function Pizza(selectedSize,selectedToppings,selectedCrust) {
+  this.selectedSize = selectedSize;
+  this.selectedToppings = selectedToppings;
+  this.selectedCrust = selectedCrust;
+}
+Pizza.prototype.sizeCost = function() {
+  var sizes = {
+    small: 600,
+    medium: 800,
+    large: 1000
+  };
+  return sizes[this.selectedSize];
+}
+Pizza.prototype.toppingsCost = function() {
+  var totalToppingsCost = 0;
+  var toppings = {
+    pepperoni: 200,
+    sausage: 200,
+    bacon: 200,
+    cheese: 200,
+    onion: 200,
+  };
+  this.selectedToppings.forEach(function(topping) {
+    totalToppingsCost += toppings[topping];
+  });
+  return totalToppingsCost;
+}
+Pizza.prototype.crustCost = function() {
+  var crusts = {
+    crispy: 100,
+    stuffed: 100,
+    gluten: 100
+  };
+  return crusts[this.selectedCrust];
+}
+Pizza.prototype.pizzaCost = function() {
+  return this.sizeCost() + this.toppingsCost() + this.crustCost();
+}
+$(document).ready(function() {
+  $('#pizza-order').submit(function(event) {
+    event.preventDefault();
+
+    var size = $("input[name=optradio1]:checked").val();
+    var crust = $("input[name=me]:checked").val();
+    var toppings = [];
+    $("input:checkbox:checked").map(function(){
+      toppings.push($(this).val());
+    });
+    var userPizza = new Pizza(size,toppings,crust);
+    var grandTotal = userPizza.pizzaCost().toFixed(2);
+    $('#totalCost').text(grandTotal);
